abf3dd5f454db9a1bb1ac3fb6222c33d87b50a23
Colden Prime <colden.prime@accenture.com>
2020-07-14 11:25:57 -0400

    Completed module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,12 +2,14 @@
   <div class="booklist">
     <h1>{{ title }}</h1>
     <ul>
-      <li v-for="book in books" :key="book.title">{{ book.title }}:{{ book.author }}</li>
+      <book-item v-for="book in books" :key="book.title" :book="book" />
     </ul>
   </div>
 </template>
 
 <script>
+import BookItem from "./BookItem";
+
 export default {
   name: "BookList",
   data() {
@@ -19,6 +21,9 @@ export default {
         { title: "Amusing Ourselves to Death", author: "Neil Postman" }
       ]
     };
+  },
+  components: {
+    BookItem
   }
 };
 </script>
