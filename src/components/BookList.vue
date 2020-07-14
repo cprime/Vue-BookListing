648e02021e070a2d990d792c95c788f8c3da7f7e
Colden Prime <colden.prime@accenture.com>
2020-07-14 10:27:19 -0400

    Completed module

---
+++ src/components/BookList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="booklist">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookList",
+  data() {
+    return {
+      title: "All Books"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+</style>
