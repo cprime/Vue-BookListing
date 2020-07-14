a066b73f440f3dacc14a18869b29ba1f015fa130
Colden Prime <colden.prime@accenture.com>
2020-07-14 11:13:53 -0400

    Completed module

---
+++ src/components/BookItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>{{ book.title }}:{{ book.author }}</li>
+</template>
+
+<script>
+export default {
+  name: "BookItem",
+  data() {
+    return {};
+  },
+  props: ["book"]
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
