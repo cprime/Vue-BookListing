23b4f87f75cd134a461d6b16de0241cdbba6c46c
Colden Prime <colden.prime@accenture.com>
2020-07-14 10:56:58 -0400

    Completed module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="booklist">
     <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="book in books" :key="book.title">{{ book.title }}:{{ book.author }}</li>
+    </ul>
   </div>
 </template>
 
@@ -9,7 +12,12 @@ export default {
   name: "BookList",
   data() {
     return {
-      title: "All Books"
+      title: "All Books",
+      books: [
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
+        { title: "American Gods", author: "Neil Gaiman" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman" }
+      ]
     };
   }
 };
@@ -20,5 +28,13 @@ export default {
 h1,
 h2 {
   font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
 }
 </style>
